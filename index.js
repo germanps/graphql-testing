d1160cda89eb3d83c3843de27d5c4eac72765bf1
German Pla <germanps@outlook.es>
2020-11-06 07:47:57 +0100

    change env

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const resolvers = require('./gql/resolver');
 
 require('dotenv').config({ path: '.env' });
 
-mongoose.connect('mongodb+srv://weatherinn:weather22@weather-inn.nmirx.mongodb.net/weatherinn', {
+mongoose.connect(process.env.BBDD, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: true,
